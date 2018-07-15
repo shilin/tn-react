ea9fa9f8846301a6a632e67818680c78b9f71ed4
Alexei Shilin <alexei.shilin@gmail.com>
2018-07-15 13:19:37 +0700

    create first component

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ console.log( MathModule.add(9, 3) );
 console.log( MathModule.subtract(9, 3) );
 console.log( MathModule.divide(9, 3) );
 console.log( MathModule.multiply(9, 3) );
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Hello from './src/Hello';
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root')
+);
