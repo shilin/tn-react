6587f838625294aa0017407e12b6839ec35aa5e0
Alexei Shilin <alexei.shilin@gmail.com>
2018-08-01 15:03:26 +0700

    remove comments

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -7,20 +7,7 @@ import CartUpdater from './CartUpdater';
 class ProductCard extends Component {
   constructor(props) {
     super(props);
-    // this.onChangeHandler = this.onChangeHandler.bind(this);
-    // this.setField = this.setField.bind(this);
-
   }
-
-  // onChangeHandler(e, name) {
-  //   console.log('changed');
-  //   console.log(name);
-  //   this.setField(e, name);
-  // }
-
-  // setField(e, name) {
-  //   this.setState({ [name]: parseInt(e.target.value) || 0 });
-  // }
 
   render() {
     const { product, addToCart } = this.props;
